7ef3d8c1ad69490d12f0ce3086e13b2825cbd6c6
wsxc94 <wsxc94@naver.com>
2020-12-18 11:22:23 +0900

    Add Manager UI + Multi Scene Loader

---
+++ Assets/Scripts/Manager/UILoaderManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UILoaderManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (SceneManager.GetSceneByName("UIScene").isLoaded == false)
+                SceneManager.LoadSceneAsync("UIScene", LoadSceneMode.Additive);
+            else
+                SceneManager.UnloadSceneAsync("UIScene");
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            if (SceneManager.GetSceneByName("CheckScene").isLoaded == false)
+                SceneManager.LoadSceneAsync("CheckScene", LoadSceneMode.Additive);
+            else
+                SceneManager.UnloadSceneAsync("CheckScene");
+        }
+
+        //if (Input.GetKeyDown(KeyCode.F3))
+        //{
+        //    if (SceneManager.GetSceneByName("Level2").isLoaded)
+        //        SceneManager.UnloadSceneAsync("Level2");
+
+        //    if (SceneManager.GetSceneByName("Level1").isLoaded == false)
+        //    {
+        //        SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Additive).completed += operation =>
+        //            SceneManager.SetActiveScene(SceneManager.GetSceneByName("Level1"));
+        //    }
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.F4))
+        //{
+        //    if (SceneManager.GetSceneByName("Level1").isLoaded)
+        //        SceneManager.UnloadSceneAsync("Level1");
+
+        //    if (SceneManager.GetSceneByName("Level2").isLoaded == false)
+        //    {
+        //        SceneManager.LoadSceneAsync("Level2", LoadSceneMode.Additive)
+        //            .completed += HandleLevel2LoadCompleted;
+        //    }
+        //}
+    }
+
+    private void HandleLevel2LoadCompleted(AsyncOperation obj)
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Level2"));
+    }
+}
