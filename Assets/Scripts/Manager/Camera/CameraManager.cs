81c42ac5935ed206a9b9ef70e627600c1c9016f3
wsxc94 <wsxc9468@gmail.com>
2020-12-22 00:14:12 +0900

    UI , Camera 수정 UI를 사용하고 있는중엔 카메라 터치가 적용되지않도록 수정

--- Assets/Scripts/Manager/Camera/CameraManager.cs
+++ Assets/Scripts/Manager/Camera/CameraManager.cs
@@ -35,12 +35,14 @@ public class CameraManager : MonoBehaviour
                 {
                     //Is mobile touch
                     // if 조이스틱 터치가 아닐경우 추가하기.
+                    if(EventSystem.current.IsPointerOverGameObject() == false)
                     return Input.touches[0].deltaPosition.x / TouchSensitivity_x;
                 }
                 else if (Input.GetMouseButton(0))
                 {
                     // is mouse click
-                    return Input.GetAxis("Mouse X");
+                    if (EventSystem.current.IsPointerOverGameObject() == false)
+                        return Input.GetAxis("Mouse X");
                 }
                 break;
             //case "rotationY":
@@ -71,20 +73,23 @@ public class CameraManager : MonoBehaviour
         float fDis = 0f;
         if (Input.touchCount == 2 && (Input.touches[0].phase == TouchPhase.Moved || Input.touches[1].phase == TouchPhase.Moved))
         {
-            m_fToucDis = (Input.touches[0].position - Input.touches[1].position).sqrMagnitude;
+            if (EventSystem.current.IsPointerOverGameObject() == false)
+            {
+                m_fToucDis = (Input.touches[0].position - Input.touches[1].position).sqrMagnitude;
 
-            fDis = (m_fToucDis - m_fOldToucDis) * 0.01f;
+                fDis = (m_fToucDis - m_fOldToucDis) * 0.01f;
 
-            // 이전 두 터치의 거리와 지금 두 터치의 거리의 차이를 FleldOfView를 차감
-            m_fFieldOfView -= fDis;
+                // 이전 두 터치의 거리와 지금 두 터치의 거리의 차이를 FleldOfView를 차감
+                m_fFieldOfView -= fDis;
 
-            // 카메라 줌 최대 범위 지정 30 ~ 100;
-            m_fFieldOfView = Mathf.Clamp(m_fFieldOfView, 30.0f, 100.0f);
+                // 카메라 줌 최대 범위 지정 30 ~ 100;
+                m_fFieldOfView = Mathf.Clamp(m_fFieldOfView, 30.0f, 100.0f);
 
-            // 확대 / 축소가 갑자기 되지않도록 보간
-            //cinemachineCamera.m_Lens.FieldOfView = Mathf.Lerp(cinemachineCamera.m_Lens.FieldOfView, m_fFieldOfView, Time.deltaTime * 5);
-            CinemachineCamera.GetRig(0).m_Lens.FieldOfView = Mathf.Lerp(CinemachineCamera.GetRig(0).m_Lens.FieldOfView, m_fFieldOfView, Time.deltaTime * 5);
-            m_fOldToucDis = m_fToucDis;
+                // 확대 / 축소가 갑자기 되지않도록 보간
+                //cinemachineCamera.m_Lens.FieldOfView = Mathf.Lerp(cinemachineCamera.m_Lens.FieldOfView, m_fFieldOfView, Time.deltaTime * 5);
+                CinemachineCamera.GetRig(0).m_Lens.FieldOfView = Mathf.Lerp(CinemachineCamera.GetRig(0).m_Lens.FieldOfView, m_fFieldOfView, Time.deltaTime * 5);
+                m_fOldToucDis = m_fToucDis;
+            }
         }
 
     }
--- Assets/Scripts/Manager/Camera/CameraZoom.cs
+++ Assets/Scripts/Manager/Camera/CameraZoom.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-
+using UnityEngine.EventSystems;
 public class CameraZoom : MonoBehaviour
 {
     CinemachineVirtualCamera cinemachineCamera;
