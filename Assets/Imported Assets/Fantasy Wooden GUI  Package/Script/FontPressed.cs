25be1950d2d8df5cd19e1c43a35ebf4b4966dbfb
fool8474 <fool8474@naver.com>
2020-12-23 18:17:28 +0900

    Asset [Fantasy Wooden GUI Package]  Add to Project

---
+++ Assets/Imported Assets/Fantasy Wooden GUI  Package/Script/FontPressed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FontPressed : MonoBehaviour {
+
+    public Text NormalBtnText;
+    public Text PressedBtnText;
+
+    public void OnClickDownBtn()
+    {
+   
+        NormalBtnText.gameObject.SetActive(false);
+        PressedBtnText.gameObject.SetActive(true);
+
+    }
+    public void OnClickUpBtn()
+    {
+
+        NormalBtnText.gameObject.SetActive(true);
+        PressedBtnText.gameObject.SetActive(false);
+    }
+}
