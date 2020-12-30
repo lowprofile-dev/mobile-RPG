24886adae30b55378ba7287a560235c84079b94a
Kijung Kim <rlwjd1504@gmail.com>
2020-12-30 23:54:34 +0900

    Added: 아이템 매니저 추가 & 플레이어가 착용중인 아이템 json 저장.

--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -3,46 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using Newtonsoft.Json;
+using System;
 
-public class ItemManager : SingletonBase<ItemManager>
+[System.Serializable]
+public class CurrentItems
 {
-    public Transform head;
-    public Transform hair;
-    public Transform headAccesories;
-    public Transform rightShoulder;
-    public Transform rightElbow;
-    public Transform rightWeapon;
-    public Transform leftShoulder;
-    public Transform leftElbow;
-    public Transform leftWeapon;
-    public Transform leftShield;
-    public Transform chest;
-    public Transform spine;
-    public Transform lowerSpine;
-    public Transform rightHip;
-    public Transform rightKnee;
-    public Transform leftHip;
-    public Transform leftKnee;
+    //투구
+    public int headAccesoriesIndex = 0;
+    //장갑
+    public int leftElbowIndex = 0;
+    public int rightElbowIndex = 0;
+    //갑옷
+    public int chestIndex = 0;
+    public int spineIndex = 0;
+    //하체
+    public int lowerSpineIndex = 0;
+    //신발
+    public int leftKneeIndex = 0;
+    public int rightKneeIndex = 0;
+}
 
-    GameObject headObject;
-    GameObject hairObject;
-    GameObject headAccesoriesObject;
-    GameObject rightShoulderObject;
-    GameObject rightElbowObject;
-    GameObject rightWeaponObject;
-    GameObject leftShoulderObject;
-    GameObject leftElbowObject;
-    GameObject leftWeaponObject;
-    GameObject leftShieldObject;
-    GameObject chestObject;
-    GameObject spineObject;
-    GameObject lowerSpineObject;
-    GameObject rightHipObject;
-    GameObject rightKneeObject;
-    GameObject leftHipObject;
-    GameObject leftKneeObject;
+public class ItemManager : MonoBehaviour
+{
+    //착용중인 아이템 인덱스
+    public CurrentItems currentItems;
 
-    [SerializeField] Dictionary<ItemData, int> playerInventory;
+    Dictionary<ItemData, int> playerInventory;
+    public Player player;
+    PartSelection playerPartSelection;
+
+    private void Start()
+    {
+        playerPartSelection = player.gameObject.GetComponent<PartSelection>();
+        LoadCurrentItems();
+        LoadInventoryData();
+    }
+
+    /// <summary>
+    /// 현재 플레이어가 착용하고 있는 아이템 저장
+    /// </summary>
+    private void SaveCurrentItems()
+    {
+        string jsonData = JsonUtility.ToJson(currentItems);
+        string path = Path.Combine(Application.dataPath, "playerCurrentItems.json");
+        File.WriteAllText(path, jsonData);
+    }
+
+    /// <summary>
+    /// 마지막으로 플레이어가 착용하고 있던 아이템 로드
+    /// </summary>
+    private void LoadCurrentItems()
+    {
+        string path = Path.Combine(Application.dataPath, "playerCurrentItems.json");
+        string jsonData = File.ReadAllText(path);
+        if (jsonData == null) return;
+        currentItems = JsonUtility.FromJson<CurrentItems>(jsonData);
+    }
 
     /// <summary>
     /// 획득한 아이템 추가!
@@ -58,16 +74,51 @@ public class ItemManager : SingletonBase<ItemManager>
         {
             playerInventory.Add(item.itemData, 1);
         }
+        SaveInventoryData();
     }
 
     public void RemoveItem(Item item)
     {
         playerInventory.Remove(item.itemData);
+        SaveInventoryData();
     }
 
+    /// <summary>
+    /// 아이템에 맞춰 플레이어에게 장착
+    /// </summary>
+    /// <param name="item">갈아낄 아이템 객체</param>
     public void SetItem(Item item)
     {
-        //  TODO : 플레이어 캐릭터와 데이터 연동.
+        switch (item.itemData.itemType)
+        {
+            case "Armor":
+                playerPartSelection.ChangeChestPart(item.itemData.itemIndex);
+                playerPartSelection.ChangeSpinePart(item.itemData.itemIndex);
+                currentItems.chestIndex = item.itemData.itemIndex;
+                currentItems.spineIndex = item.itemData.itemIndex;
+                break;
+            case "Bottom":
+                playerPartSelection.ChangeLowerSpinePart(item.itemData.itemIndex);
+                currentItems.lowerSpineIndex = item.itemData.itemIndex;
+                break;
+            case "Helmet":
+                playerPartSelection.ChangeHeadAccesoriesPart(item.itemData.itemIndex);
+                currentItems.headAccesoriesIndex = item.itemData.itemIndex;
+                break;
+            case "Gloves":
+                playerPartSelection.ChangeLeftElbowPart(item.itemData.itemIndex);
+                playerPartSelection.ChangeRightElbowPart(item.itemData.itemIndex);
+                currentItems.leftElbowIndex = item.itemData.itemIndex;
+                currentItems.rightElbowIndex = item.itemData.itemIndex;
+                break;
+            case "Boot":
+                playerPartSelection.ChangeLeftKneePart(item.itemData.itemIndex);
+                playerPartSelection.ChangeRightKneePart(item.itemData.itemIndex);
+                currentItems.leftKneeIndex = item.itemData.itemIndex;
+                currentItems.rightKneeIndex = item.itemData.itemIndex;
+                break;
+        }
+        SaveCurrentItems();
     }
 
     /// <summary>
@@ -76,7 +127,7 @@ public class ItemManager : SingletonBase<ItemManager>
     [ContextMenu("Save Inventory Data to Json")]
     public void SaveInventoryData()
     {
-        string jsonData = JsonConvert.SerializeObject(playerInventory);
+        string jsonData = JsonConvert.SerializeObject(playerInventory, Formatting.Indented);
         string path = Path.Combine(Application.dataPath, "itemDatabase.json");
         File.WriteAllText(path, jsonData);
     }
@@ -89,6 +140,7 @@ public class ItemManager : SingletonBase<ItemManager>
     {
         string path = Path.Combine(Application.dataPath, "itemDatabase.json");
         string jsonData = File.ReadAllText(path);
-        playerInventory = JsonUtility.FromJson<Dictionary<ItemData, int>>(jsonData);
+        if (jsonData == null) return;
+        playerInventory = JsonConvert.DeserializeObject<Dictionary<ItemData, int>>(jsonData);
     }
 }
