61822ca3bcd4b2f8b7c7c87496845470f075e121
Kijung Kim <rlwjd1504@gmail.com>
2021-01-01 04:05:43 +0900

    Add: 인벤토리 UI에 보유한 아이템 데이터 로딩하는 기능 추가.

---
+++ Assets/Scripts/UI/Inventory UI/ItemSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    public GameObject itemIcon;
+    public GameObject itemQuantity;
+
+    private void Awake()
+    {
+        itemIcon = gameObject.transform.GetChild(0).gameObject;
+        itemQuantity = gameObject.transform.GetChild(1).gameObject;
+    }
+
+    public void SetIcon(Transform model)
+    {
+        //Instantiate(RuntimePreviewGenerator.GenerateModelPreview(model), itemIcon);
+        Texture2D texture = RuntimePreviewGenerator.GenerateModelPreview(model);
+        Rect rect = new Rect(0, 0, texture.width, texture.height);
+        itemIcon.GetComponent<Image>().sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+    }
+
+    public void SetQuantity(int quantity)
+    {
+        itemQuantity.GetComponent<TextMeshProUGUI>().SetText(quantity.ToString());
+    }
+}
