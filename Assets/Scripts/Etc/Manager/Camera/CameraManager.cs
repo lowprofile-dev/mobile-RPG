1fc4f48352703680fb1544d6a69b780b7476aa99
wsxc94 <wsxc9468@gmail.com>
2021-01-03 20:19:11 +0900

    던전 , 마을 카메라 변경 UI 줌 , 터치 상호작용 업데이트

--- Assets/Scripts/Etc/Manager/Camera/CameraManager.cs
+++ Assets/Scripts/Etc/Manager/Camera/CameraManager.cs
@@ -8,21 +8,23 @@ using UnityEngine.EventSystems;
 public class CameraManager : MonoBehaviour
 {
 
-    CinemachineFreeLook CinemachineCamera;
+    [SerializeField] CinemachineFreeLook CinemachineCamera;
+    SimpleInputNamespace.Joystick joystick;
 
-    void OnEnable()
+    private void Start()
     {
         CinemachineCore.GetInputAxis = this.HandleAxisInputDelegate;
-        CinemachineCamera = GetComponent<CinemachineFreeLook>();
+        CinemachineCamera.m_CommonLens = true;
     }
 
     public void InitCameraManager()
     {
-
+      
     }
 
     private void Update()
     {
+        if(joystick == null) joystick = GameObject.FindGameObjectWithTag("Joystick").GetComponent<SimpleInputNamespace.Joystick>();
         ZoomInOut();
     }
 
@@ -30,19 +32,28 @@ public class CameraManager : MonoBehaviour
     //public float TouchSensitivity_y = 10f;
 
     float m_fOldToucDis = 0f;       // 터치 이전 거리를 저장
-    float m_fFieldOfView = 5;     // 카메라의 FieldOfView의 기본값
+    float m_fFieldOfView = 7;     // 카메라의 FieldOfView의 기본값
 
     private float HandleAxisInputDelegate(string axisName)
     {
         switch (axisName)
         {
             case "rotationX":
-                if (Input.touchCount > 0)
+                if(Input.touchCount == 1)
+                {
+                    if (!joystick.getHold())
+                    {
+                        if (EventSystem.current.IsPointerOverGameObject() == false)
+                            return Input.touches[0].deltaPosition.x / TouchSensitivity_x;
+                    }
+                    
+                }
+                else if (Input.touchCount > 0)
                 {
                     Touch[] touch = Input.touches;
                     //Is mobile touch
                     for (int i = 0; i < touch.Length; i++)
-                    {
+                    {                    
                         if ((touch[i].phase == TouchPhase.Moved || touch[i].phase == TouchPhase.Stationary) && EventSystem.current.IsPointerOverGameObject(Input.GetTouch(i).fingerId) == false)
                             return Input.touches[i].deltaPosition.x / TouchSensitivity_x;
                     }           
@@ -51,20 +62,28 @@ public class CameraManager : MonoBehaviour
                 else if (Input.GetMouseButton(0))
                 {
                     // is mouse click
-                    if (EventSystem.current.IsPointerOverGameObject() == false)
-                        return Input.GetAxis("Mouse X");
+                    if (!joystick.getHold())
+                    {
+                        
+                        if (EventSystem.current.IsPointerOverGameObject() == false)
+                            return Input.GetAxis("Mouse X");
+                    }
                 }
 #endif
 
 #if (UNITY_ANDROID || UNITY_IPHONE) && !UNITY_EDITOR
                 else if (Input.GetTouch(0).phase == TouchPhase.Began)
                 {
-                    // mobile touch click
-                    if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) == false)
-                        return Input.GetAxis("Mouse X");
+                
+                    if(!joystick.getHold())
+                    {
+                         Debug.Log("이거다");
+                       if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) == false)
+                           return Input.GetAxis("Mouse X");
+                    }
                 }
 #endif
-                
+
                 break;
             default:
 #if UNITY_EDITOR
@@ -80,9 +99,10 @@ public class CameraManager : MonoBehaviour
     {
         float m_fToucDis = 0f;
         float fDis = 0f;
-        if (Input.touchCount == 2 && (Input.touches[0].phase == TouchPhase.Moved || Input.touches[1].phase == TouchPhase.Moved))
+        if (!joystick.getHold() && Input.touchCount == 2 && (Input.touches[0].phase == TouchPhase.Moved || Input.touches[1].phase == TouchPhase.Moved))
         {
-            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) == false)
+            
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) == false && EventSystem.current.IsPointerOverGameObject(Input.GetTouch(1).fingerId) == false)
             {
                 m_fToucDis = (Input.touches[0].position - Input.touches[1].position).sqrMagnitude;
 
@@ -91,16 +111,17 @@ public class CameraManager : MonoBehaviour
                 // 이전 두 터치의 거리와 지금 두 터치의 거리의 차이를 FleldOfView를 차감
                 m_fFieldOfView -= fDis;
 
-                // 카메라 줌 최대 범위 지정 30 ~ 100;
+                // 카메라 줌 최대 범위 지정 2 ~ 7;
                 m_fFieldOfView = Mathf.Clamp(m_fFieldOfView, 2, 7);
 
                 // 확대 / 축소가 갑자기 되지않도록 보간
-                //cinemachineCamera.m_Lens.FieldOfView = Mathf.Lerp(cinemachineCamera.m_Lens.FieldOfView, m_fFieldOfView, Time.deltaTime * 5);
-                CinemachineCamera.GetRig(0).m_Lens.OrthographicSize = Mathf.Lerp(CinemachineCamera.GetRig(0).m_Lens.OrthographicSize, m_fFieldOfView, Time.deltaTime * 5);
-                
+                //CinemachineCamera.m_Lens.FieldOfView = Mathf.Lerp(CinemachineCamera.m_Lens.FieldOfView, m_fFieldOfView, Time.deltaTime * 5);
+                //CinemachineCamera.GetRig(0).m_Lens.OrthographicSize = Mathf.Lerp(CinemachineCamera.GetRig(0).m_Lens.OrthographicSize, m_fFieldOfView, Time.deltaTime * 5);
+                CinemachineCamera.m_Lens.OrthographicSize = Mathf.Lerp(CinemachineCamera.m_Lens.OrthographicSize, m_fFieldOfView, Time.deltaTime * 5);
                 m_fOldToucDis = m_fToucDis;
             }
         }
 
     }
+
 }
