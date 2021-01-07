27e0c684f5e64797b5317c26797b274fabb24de6
wsxc94 <wsxc9468@gmail.com>
2021-01-07 18:28:52 +0900

    Boss Monster Update

--- Assets/Scripts/Unit/LivingEntity/Monster/BossSkeletonPase2.cs
+++ Assets/Scripts/Unit/LivingEntity/Monster/BossSkeletonPase2.cs
@@ -12,7 +12,10 @@ public class BossSkeletonPase2 : MonsterAction
     [SerializeField] private GameObject _baseMeleeAttackPrefab;
 
     AttackType attackType;
-    [SerializeField] private GameObject SkillRange;
+    [SerializeField] private GameObject JumpSkillRange;
+    [SerializeField] private GameObject ShokeSkillRange;
+    private GameObject currentTarget;
+
     string currentAnimation;
 
     private void OnDrawGizmos()
@@ -26,25 +29,29 @@ public class BossSkeletonPase2 : MonsterAction
 
     protected override void DoAttack()
     {
-        base.DoAttack();
+          
+        GameObject obj = ObjectPoolManager.Instance.GetObject(_baseMeleeAttackPrefab);
+        obj.transform.SetParent(this.transform);
+        obj.transform.position = _baseMeleeAttackPos.position;
 
-        if (_attackType == 0 || _attackType == 1)
-        {
-            GameObject obj = ObjectPoolManager.Instance.GetObject(_baseMeleeAttackPrefab);
-            obj.transform.SetParent(this.transform);
-            obj.transform.position = _baseMeleeAttackPos.position;
+        Attack atk = obj.GetComponent<Attack>();
+        atk.SetParent(gameObject);
+        atk.PlayAttackTimer(1);
 
-            Attack atk = obj.GetComponent<Attack>();
-            atk.SetParent(gameObject);
-            atk.PlayAttackTimer(1);
-        }
+        
+        StopCoroutine(_attackCoroutine);
+        _attackCoroutine = null;
+        _readyCast = false;
+
+        CameraManager.Instance.ShakeCamera(3, 1, 0.5f);
+        currentTarget = _target;
+        ChangeState(STATE.STATE_TRACE);
 
 
     }
     protected override void SetAttackAnimation()
     {
-        transform.LookAt(_target.transform);
-
+        
         switch (attackType)
         {
             case AttackType.JUMP_ATTACK:
@@ -79,6 +86,11 @@ public class BossSkeletonPase2 : MonsterAction
     {
         base.SpawnExit();
     }
+    private void AttackCorotineInit()
+    {
+        _attackCoroutine = null;
+        ChangeState(STATE.STATE_TRACE);
+    }
 
     protected override void CastStart()
     {
@@ -86,14 +98,16 @@ public class BossSkeletonPase2 : MonsterAction
         // 플레이어가 기절상태나 넘어짐 상태면 우선 공격 모션 2개 있음.
         // if(_target.getState? == 기절) attackType = AttackType.~~~~
 
+        if (_attackCoroutine != null) Invoke("AttackCorotineInit", 1.5f);
+
         int proc = UnityEngine.Random.Range(0, 100);
 
-        if (proc <= 25)
+        if (proc <= 50)
         {
             _castTime = 1.5f;
             attackType = AttackType.JUMP_ATTACK;
         }
-        else if (proc <= 50)
+        else if (proc <= 100)
         {
             _castTime = 1f;
             attackType = AttackType.SHOCK_WAVE;
@@ -140,55 +154,138 @@ public class BossSkeletonPase2 : MonsterAction
         _currentState = STATE.STATE_NULL;
         ChangeState(STATE.STATE_IDLE);
         _navMeshAgent.enabled = true;
+        _navMeshAgent.speed = _moveSpeed;
+        currentTarget = _target;
+
     }
 
     protected override void AttackExit()
     {
         _monster.MyAnimator.ResetTrigger(currentAnimation);
-        if (_attackCoroutine != null) StopCoroutine(_attackCoroutine);
+        //if (_attackCoroutine != null)
+        //{
+        //    StopCoroutine(_attackCoroutine);
+        //    _attackCoroutine = null;
+        //}
+            
     }
 
     public override void MoveToTarget()
     {
-        _navMeshAgent.SetDestination(_target.transform.position);
+        
+        _navMeshAgent.SetDestination(currentTarget.transform.position);
 
-        ChangeState(STATE.STATE_ATTACK);
+        if (Vector3.Distance(_target.transform.position, _monster.transform.position) < _attackRange)
+        {
+            ChangeState(STATE.STATE_CAST);            
+        }
 
     }
 
     protected override IEnumerator AttackTarget()
     {
+        Debug.Log("AttackCorotine");
+        //CastStart();
+
         while (true)
         {
             yield return null;
 
+            AttackAction();
             
-                yield return new WaitForSeconds(_attackSpeed - _monster.MyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
-                SetAttackType();
-                SetAttackAnimation();
+            //StartCoroutine(DoAttackAction());
 
-                LookTarget();
+            yield return new WaitForSeconds(_attackSpeed);
+            SetAttackAnimation();
 
-                // 사운드 재생
+            // 사운드 재생
 
-                StartCoroutine(DoAttackAction());
+            //레인지 끄기
+            
+            
+            yield return new WaitForSeconds(_monster.MyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
 
-                yield return new WaitForSeconds(_monster.MyAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            //_monster.MyAnimator.ResetTrigger(currentAnimation); // 애니메이션의 재시작 부분에 Attack이 On이 되야함.
 
-                _monster.MyAnimator.ResetTrigger(currentAnimation); // 애니메이션의 재시작 부분에 Attack이 On이 되야함.
+            _navMeshAgent.speed = _moveSpeed;
+            _readyCast = false;
+            if (!_readyCast && ToCast()) break;
+            ChangeState(STATE.STATE_TRACE);
 
-                _readyCast = false;
-                if (!_readyCast && ToCast()) break;
-           
-                ChangeState(STATE.STATE_TRACE);
-                break;
+            break;
             
         }
     }
-    public override IEnumerator DoAttackAction()
+    private void AttackAction()
     {
-        yield return null;
-
+        switch (attackType)
+        {
+            case AttackType.JUMP_ATTACK:
+                StartCoroutine(JumpAction());
+                break;
+            case AttackType.SHOCK_WAVE:
+                StartCoroutine(ShokeAction());
+                break;
+            case AttackType.SHOCK_WAVE2:
+                break;
+            case AttackType.DASH_ATTACK:
+                break;
+            case AttackType.LEFT_ATTACK:
+                break;
+            default:
+                break;
+        }
 
     }
+    private IEnumerator JumpAction()
+    {
+        _navMeshAgent.speed = _moveSpeed * 1.5f;
+        GameObject range = ObjectPoolManager.Instance.GetObject(JumpSkillRange);
+        range.transform.position = _target.transform.position;
+        currentTarget = range;
+        transform.LookAt(range.transform.position);
+
+        yield return new WaitForSeconds(_attackSpeed);
+
+        ObjectPoolManager.Instance.ReturnObject(range);
+    }
+
+    private IEnumerator ShokeAction()
+    {
+
+        GameObject range = ObjectPoolManager.Instance.GetObject(ShokeSkillRange);
+
+        range.GetComponent<BossSkillRange>().setFollow();
+              
+        yield return new WaitForSeconds(_attackSpeed);
+          
+
+        ObjectPoolManager.Instance.ReturnObject(range);
+    }
+
+
+    protected override void TraceStart()
+    {
+        _monster.MyAnimator.SetTrigger("Walk");
+        _navMeshAgent.isStopped = false;
+    }
+
+    protected override void AttackStart()
+    {
+        if (!_readyCast && ToCast()) return;
+        else
+        {
+            if (_attackCoroutine == null)
+                _attackCoroutine = StartCoroutine(AttackTarget());
+            else
+                ChangeState(STATE.STATE_IDLE);
+        }
+        
+    }
+
+    public override void Damaged(float dmg, bool SetAnimation = false)
+    {
+        base.Damaged(dmg, false);
+    }
+
 }
