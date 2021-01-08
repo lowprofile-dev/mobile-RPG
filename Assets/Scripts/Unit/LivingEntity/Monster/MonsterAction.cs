7bf99f3e662544a32579ab1cf1e7118d82cbe12d
wsxc94 <wsxc9468@gmail.com>
2021-01-09 03:05:38 +0900

    Monster STATE name update

--- Assets/Scripts/Unit/LivingEntity/Monster/MonsterAction.cs
+++ Assets/Scripts/Unit/LivingEntity/Monster/MonsterAction.cs
@@ -28,8 +28,7 @@ public class MonsterAction : MonoBehaviour
 
     // 기타 변수
     protected MONSTER_STATE _currentState;                  // 현재 상태
-    protected STATE _currentState;                  // 현재 상태
-    public STATE currentState { get { return _currentState; } }
+    public MONSTER_STATE currentState { get { return _currentState; } }
 
     protected float _distance;                      // 타겟과의 거리
     protected float _traceTimer;                    // 추적 이후 경과한 시간
