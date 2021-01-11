cf7e44db6ef2c154575fafd66e766875f4b416b9
Kijung Kim <rlwjd1504@gmail.com>
2021-01-11 19:13:29 +0900

    던전 문 안열리는 버그 수정(스폰 버그) & 로비씬 스카이박스 수정

--- Assets/Scripts/Map/DungeonRoom.cs
+++ Assets/Scripts/Map/DungeonRoom.cs
@@ -336,18 +336,29 @@ public class DungeonRoom : MonoBehaviour
         {
             return;
         }
-        for (int i = 0; i < nMonsters; i++)
-        {
-            spawnPointIndex = random.Next(monsterSpawnPoints.Count);
-            monsterIndex = random.Next(monsterPrefabs.Count);
-            var monster = Instantiate(monsterPrefabs[monsterIndex]);
-            monster.GetComponent<NavMeshAgent>().enabled = false;
-            monster.transform.position = monsterSpawnPoints[spawnPointIndex].transform.TransformPoint(0, 0, 0);
-            //monster.transform.SetParent(null);
-            monster.GetComponent<NavMeshAgent>().enabled = true;
-            nMonsterSpawned += nMonsters;
-            monsters.Add(monster);
-        }
+        spawnPointIndex = random.Next(monsterSpawnPoints.Count);
+        //spawnPointIndex = i;
+        monsterIndex = random.Next(monsterPrefabs.Count);
+        var monster = Instantiate(monsterPrefabs[monsterIndex]);
+        monster.GetComponent<NavMeshAgent>().enabled = false;
+        monster.transform.position = monsterSpawnPoints[spawnPointIndex].transform.TransformPoint(0, 0, 0);
+        //monster.transform.SetParent(null);
+        monster.GetComponent<NavMeshAgent>().enabled = true;
+        nMonsterSpawned += 1;
+        monsters.Add(monster);
+        //for (int i = 0; i < nMonsters; i++)
+        //{
+        //    spawnPointIndex = random.Next(monsterSpawnPoints.Count);
+        //    //spawnPointIndex = i;
+        //    monsterIndex = random.Next(monsterPrefabs.Count);
+        //    var monster = Instantiate(monsterPrefabs[monsterIndex]);
+        //    monster.GetComponent<NavMeshAgent>().enabled = false;
+        //    monster.transform.position = monsterSpawnPoints[spawnPointIndex].transform.TransformPoint(0, 0, 0);
+        //    //monster.transform.SetParent(null);
+        //    monster.GetComponent<NavMeshAgent>().enabled = true;
+        //    nMonsterSpawned += 1;
+        //    monsters.Add(monster);
+        //}
     }
 
     IEnumerator SpawnMonsterCoroutine()
