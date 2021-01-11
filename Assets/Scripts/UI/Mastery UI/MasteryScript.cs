443f1372208cc07eb78bb2ecd2043e9e99fac187
DESKTOP-OT8343G\dmlru <ug4306@gmail.com>
2021-01-11 15:07:47 +0900

    + 마스터리 레벨 제한 추가 + 강화창 날라간거 복구 + 마스터리 DB 작성

--- Assets/Scripts/UI/Mastery UI/MasteryScript.cs
+++ Assets/Scripts/UI/Mastery UI/MasteryScript.cs
@@ -16,28 +16,16 @@ public class MasteryScript : MonoBehaviour
     GameObject[] infoWindow;
     PlayerMasteryData[] masteryData;
     private bool isInit = false;
+    private int levelLimit;
     // Start is called before the first frame update
     void Start()
     {
         upSkill = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Toggle>();
         downSkill = transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Toggle>();
         masteryData = new PlayerMasteryData[2];
+        levelLimit = 5;
         upPanel.SetActive(false);
         downPanel.SetActive(false);
-        //infoWindow = new GameObject[2];
-        //infoWindow[0] = Instantiate(masteryInfo);
-        //infoWindow[0].transform.SetParent(parent.transform);
-        //
-        //Debug.Log(infoWindow[0].transform.parent.name);
-        //infoWindow[0].SetActive(false);
-        //infoWindow[0].transform.position = upSkill.transform.position;
-        //
-        //infoWindow[1] = Instantiate(masteryInfo);
-        //infoWindow[1].transform.SetParent(parent.transform);
-        //Debug.Log(infoWindow[1].transform.parent.name);
-        //
-        //infoWindow[0].SetActive(false);
-        //infoWindow[1].transform.position = upSkill.transform.position;
 
     }
 
@@ -52,51 +40,86 @@ public class MasteryScript : MonoBehaviour
     {
         if(MasteryManager.Instance != null && isInit == false)
         {
-           // switch (gameObject.name)
-           // {
-           //     case "Level5":
-                    masteryData[0] = MasteryManager.Instance.masteryDictionary[1];
-                    masteryData[1] = MasteryManager.Instance.masteryDictionary[2];
-           //         break;
-           // }
+            switch (gameObject.name)
+            {
+                case "Level5":
+                  masteryData[0] = MasteryManager.Instance.masteryDictionary[1];
+                  masteryData[1] = MasteryManager.Instance.masteryDictionary[2];
+                  levelLimit = 5;
+                  break;
+                case "Level10":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[3];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[4];
+                    levelLimit = 10;
+                    break;
+                case "Level15":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[5];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[6];
+                    levelLimit = 15;
+                    break;
+                case "Level20":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[7];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[8];
+                    levelLimit = 20;
+                    break;
+                case "Level25":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[9];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[10];
+                    levelLimit = 25;
+                    break;
+                case "Level30":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[11];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[12];
+                    levelLimit = 30;
+                    break;
+                case "Level35":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[13];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[14];
+                    levelLimit = 35;
+                    break;
+                case "Level40":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[15];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[16];
+                    levelLimit = 40;
+                    break;
+                case "Level45":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[17];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[18];
+                    levelLimit = 45;
+                    break;
+                case "Level50":
+                    masteryData[0] = MasteryManager.Instance.masteryDictionary[19];
+                    masteryData[1] = MasteryManager.Instance.masteryDictionary[20];
+                    levelLimit = 50;
+                    break;
+            }
+
             isInit = true;
         }
     }
 
     public void ToggleCheck()
     {
- /*
-        if(upSkill.isOn == true && downSkill.isOn == false)
+        if(levelLimit <= MasteryManager.Instance.currentMastery.currentMasteryLevel)
         {
-            downSkill.enabled = false;
-            downSkill.isOn = false;
-        }
-        else if (upSkill.isOn == false && downSkill.isOn == true)
-        {
-            upSkill.enabled = false;
-            upSkill.isOn = false;
+            if (upSkill.isOn)
+            {
+                downPanel.SetActive(true);
+            }
+            else if (downSkill.isOn)
+            {
+                upPanel.SetActive(true);
+            }
+            else
+            {
+                upPanel.SetActive(false);
+                downPanel.SetActive(false);
+            }
         }
         else
-        {
-            downSkill.enabled = true;
-            upSkill.enabled = true;
-            downSkill.isOn = false;
-            upSkill.isOn = false;
-        
-        }
-   */
-        if(upSkill.isOn)
-        {
-            downPanel.SetActive(true);
-        }
-        else if(downSkill.isOn)
         {
             upPanel.SetActive(true);
-        }
-        else
-        {
-            upPanel.SetActive(false);
-            downPanel.SetActive(false);
+            downPanel.SetActive(true);
         }
 
     }
