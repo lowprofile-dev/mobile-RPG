0dc0ef813f87b132879ffad4f986e3b19d14e2e8
wsxc94 <wsxc9468@gmail.com>
2021-01-12 22:53:49 +0900

    보스 공격범위 , 능력치 너프

--- Assets/Resources/Prefab/Effect/SkillEffect/Monster/Attacks/Boss/SkeletonBaseShokeWave1.cs
+++ Assets/Resources/Prefab/Effect/SkillEffect/Monster/Attacks/Boss/SkeletonBaseShokeWave1.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class SkeletonBaseShokeWave1 : BossAttack
 {
-   
-    protected override void SetLocalRotation(GameObject eft , GameObject _target)
+
+    public override void OnLoad(GameObject start, GameObject target)
     {
-        Vector3 dir = transform.forward + _target.transform.right;
-        float pos = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
-        angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, pos, ref velocity, 1f);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        GameObject effect = ObjectPoolManager.Instance.GetObject(_particleEffectPrefab);
+
+        effect.transform.position = new Vector3(target.transform.position.x, start.transform.position.y, target.transform.position.z);
+
+    }
+    public override void SetParent(GameObject parent, Transform target)
+    {
+        _baseParent = parent;
+        transform.SetParent(target);
+        transform.localPosition = Vector3.zero;
     }
 }
