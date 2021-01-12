3bc6b657bd84421b419084931e6e82c37cfd71c9
fool8474 <fool8474@naver.com>
2021-01-12 22:57:13 +0900

    퀘스트 연출용 UI 패널 만들어놓음

---
+++ Assets/Scripts/UI/Navi/Pattern/View/QuestEffectUIView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestEffectUIView : View
+{
+    public override void UIExit()
+    {
+        base.UIExit();
+    }
+
+    public override void UIStart()
+    {
+        base.UIStart();
+    }
+
+    public override void UIUpdate()
+    {
+        base.UIUpdate();
+    }
+}
