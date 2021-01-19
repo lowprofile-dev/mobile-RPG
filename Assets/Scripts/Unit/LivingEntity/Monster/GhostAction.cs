e521ab0ff4b7b5d6521354fc88728d0c91ce0810
DESKTOP-OT8343G\dmlru <ug4306@gmail.com>
2021-01-20 03:26:12 +0900

    유령 몬스터 추가

---
+++ Assets/Scripts/Unit/LivingEntity/Monster/GhostAction.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+public class GhostAction : MonsterAction
+{
+    bool canPanic;
+
+    [SerializeField] private Transform _baseMeleeAttackPos;
+    [SerializeField] private GameObject _baseMeleeAttackPrefab;
+
+    /////////// 기본 /////////////
+
+    /// <summary>
+    /// 패닉이 추가되었으므로 오버라이딩
+    /// </summary>
+    public override void InitObject()
+    {
+        base.InitObject();
+        canPanic = true;
+    }
+
+    /////////// 상태 관련 /////////////
+
+    /////////// 스폰 관련 /////////////
+
+    /////////// 대기 관련 /////////////
+
+    /////////// 두리번거리기 관련 /////////////
+
+    /////////// 탐색 관련 /////////////
+
+    protected override void FindStart()
+    {
+        base.FindStart();
+
+        if (canPanic)
+        {
+            _monster.myAnimator.SetTrigger("Panic");
+        }
+    }
+
+    protected override bool CheckFindAnimationOver()
+    {
+        if (canPanic) return CheckAnimationOver("Panic", 1.0f);
+        else return true;
+    }
+
+    protected override void FindExit()
+    {
+        base.FindExit();
+        canPanic = false;
+    }
+
+    /////////// 추적 관련 /////////////
+
+    protected override void DoReturn()
+    {
+        base.DoReturn();
+        canPanic = true;
+    }
+
+    /////////// 공격 관련 /////////////
+
+    protected override void DoAttack()
+    {
+        GameObject obj = ObjectPoolManager.Instance.GetObject(_baseMeleeAttackPrefab);
+        obj.transform.SetParent(this.transform);
+        obj.transform.position = _baseMeleeAttackPos.position;
+
+        Attack atk = obj.GetComponent<Attack>();
+        atk.SetParent(gameObject);
+        atk.PlayAttackTimer(0.3f);
+    }
+
+    /////////// 캐스팅 관련 /////////////
+}
